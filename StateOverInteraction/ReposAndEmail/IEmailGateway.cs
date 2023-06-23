a81e8fe7f762860aeb59e286a469bf9ff1cc1712
Alistair Cooke <alistair.cooke77@gmail.com>
2023-06-23 12:10:30 +0100

    IEmailGateway

---
+++ StateOverInteraction/ReposAndEmail/IEmailGateway.cs
@@ -0,0 +1,27 @@
+﻿namespace StateOverInteraction.ReposAndEmail;
+
+public interface IEmailGateway
+{
+    void Send(EmailMessage message);
+}
+
+public class EmailMessage
+{
+}
+
+public interface IOrderRepository
+{
+    Order GetOrderById(int orderId);
+    IReadOnlyList<Order> GetActiveOrdersForCustomer(int customerId);
+    void Save(Order order);
+}
+
+public class Order
+{
+    
+}
+
+public class OrderService
+{
+    void Place
+}
